52449210d8242acbfc61e4cc5e745c2c4f22f30a
thang <thangdaoduc.ict@gmail.com>
2026-04-14 17:32:51 +0700

    update: push full changes

--- dist/shared/middleware/not-found.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundMiddleware = notFoundMiddleware;
-function notFoundMiddleware(req, res, next) {
-    res.status(404).json({
-        success: false,
-        message: "API not found"
-    });
-}
